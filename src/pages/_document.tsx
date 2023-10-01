23e69cb5d3514a17812a6d06e79e98f622e76322
kutarkutakuta <kutarkutakuta@gmail.com>
2023-10-01 23:19:05 +0900

    ヘルプとか

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,83 @@ const MyDocument = () => (
   <Html lang="ja">
     <Head>
       <meta title="Memory Cart"></meta>
+      <meta
+        name="description"
+        content="買い物リストをメモしたり共有したり。面倒な登録は一切不要ですぐ使えます。"
+      />
+      {/* <!-- Facebook:カード用--> */}
+      <meta property="og:url" content="https://okaimono.onrender.com" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Memory Cart" />
+      <meta
+        property="og:description"
+        content="買い物リストをメモしたり共有したり。面倒な登録は一切不要ですぐ使えます。"
+      />
+      <meta
+        property="og:image"
+        content="https://renkei-navi.onrender.com/assets/icons/icon-128x128.png"
+      />
+      {/* <!-- twitter:カード用--> */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@kutakutar_ff11" />
+      <meta name="twitter:domain" content="okaimono.onrender.com" />
+      <meta name="twitter:url" content="https://okaimono.onrender.com" />
+      <meta name="twitter:title" content="Memory Cart" />
+      <meta
+        name="twitter:description"
+        content="買い物リストをメモしたり共有したり。面倒な登録は一切不要ですぐ使えます。"
+      />
+      <meta
+        name="twitter:image"
+        content="https://okaimono.onrender.com/assets/icons/icon-128x128.png"
+      />
+
+      <meta
+        name="google-site-verification"
+        content="eKWsAU6fruzFLUVXCaNJ1VF4-8tcVAZrHDUYUNmsGOE"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="assets/icons/icon-16x16.png"
+        sizes="16x16"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="assets/icons/icon-32x32.png"
+        sizes="32x32"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="assets/icons/icon-48x48.png"
+        sizes="48x48"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="assets/icons/icon-64x64.png"
+        sizes="64x64"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="assets/icons/icon-128x128.png"
+        sizes="128x128"
+      />
+      <link rel="apple-touch-icon" href="assets/icons/icon-128x128.png" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&amp;display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet"
+      />
+      <link rel="manifest" href="manifest.webmanifest" />
+      <meta name="theme-color" content="#1976d2" />
     </Head>
     <body>
       <Main />
--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 
 import MyHeader from "@/components/MyHeader";
+import { Divider } from "antd";
 
 const HomePage = () => {
   return (
@@ -11,9 +12,7 @@ const HomePage = () => {
       <main>
         <h3>アプリケーションのプライバシーポリシー</h3>
         <div>このアプリケーションは、個人情報を収集したり公開しません。</div>
-        <br/>
-        <hr />
-        <br/>
+        <Divider />
         <h3>Application privacy policy</h3>
         <div>
           This application does not collect or publish any personal information.
